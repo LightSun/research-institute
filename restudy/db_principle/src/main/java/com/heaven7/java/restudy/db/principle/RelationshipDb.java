59a9ccb477413c4429b61dc378f3a03e00b959e7
LightSun <532278976@qq.com>
2017-09-29 18:29:51 +0800

    add db study

---
+++ restudy/db_principle/src/main/java/com/heaven7/java/restudy/db/principle/RelationshipDb.java
@@ -0,0 +1,29 @@
+package com.heaven7.java.restudy.db.principle;
+
+/**
+ *  关系数据库原理： 选择，投影，连接, 除
+ *  ps: 选择跟一般的数据库一样。根据条件选择满足的元组
+ * Created by heaven7 on 2017/9/29 0029.
+ */
+public class RelationshipDb {
+
+  //从关系 R 上选取若干属性列 A，并删除重复行，组成新的关系
+    public void projection(){
+
+    }
+
+    /**
+     * 它是从两个关系 R 和 S 的笛卡尔积 R×S 中选取属性间满足一定条件
+     的元组，构成新的关系。
+     */
+    public void join(){
+
+    }
+
+    /**
+     * http://blog.csdn.net/u012411414/article/details/41048861
+     */
+    public void divide(){
+
+    }
+}
