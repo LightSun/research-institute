d712d1205dfb917595550f6bebb975e7612fe55c
LightSun <donshine723@gmail.com>
2018-10-05 22:42:41 +0800

    change little for android

--- java/common-montage/src/main/java/com/heaven7/utils/ConcurrentManager.java
+++ java/common-montage/src/main/java/com/heaven7/utils/ConcurrentManager.java
@@ -1,13 +1,24 @@
 package com.heaven7.utils;
 
+import com.heaven7.java.base.util.Platforms;
 import com.heaven7.java.base.util.threadpool.ThreadPoolExecutor2;
 
 import java.util.concurrent.*;
 
 public class ConcurrentManager {
 
-    private static final int CORE_SIZE = 8;
-    private static final int MAX_SIZE  = 16;
+    private static final int CORE_SIZE;
+    private static final int MAX_SIZE;
+
+    static {
+        if(Platforms.isAndroid()){
+            CORE_SIZE = 3;
+            MAX_SIZE = 5;
+        }else{
+            CORE_SIZE = 8;
+            MAX_SIZE = 16;
+        }
+    }
 
   /*  private final ExecutorService mCoreService = new ThreadPoolExecutor(CORE_SIZE,
             MAX_SIZE, 60,
