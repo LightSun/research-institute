3e5061a5633eff7983426da93ef0c6ed5bf058fa
lightsun <978136772@qq.com>
2018-10-15 14:16:30 +0800

    change special effect cons

--- java/ffmpeg-merge-video/src/main/java/com/heaven7/ve/cross_os/EffectsHelper.java
+++ java/ffmpeg-merge-video/src/main/java/com/heaven7/ve/cross_os/EffectsHelper.java
@@ -42,20 +42,61 @@ public class EffectsHelper {
         int type;
         switch (val) {
             case "slow_zoom_in":
-                type = ISpecialEffectInfo.TYPE_SLOW_ZOOM_IN;
+                type = ISpecialEffectInfo.TYPE_ZOOM_IN;
                 break;
             case "slow_zoom_out":
-                type = ISpecialEffectInfo.TYPE_SLOW_ZOOM_OUT;
+                type = ISpecialEffectInfo.TYPE_ZOOM_OUT;
                 break;
             case "left_move":
-                type = ISpecialEffectInfo.TYPE_LEFT_MOVE;
+                type = ISpecialEffectInfo.TYPE_LEFT_TRANSLATION;
                 break;
             case "right_move":
-                type = ISpecialEffectInfo.TYPE_RIGHT_MOVE;
+                type = ISpecialEffectInfo.TYPE_RIGHT_TRANSLATION;
                 break;
+
             case "very_slow":
-                type = ISpecialEffectInfo.TYPE_VERY_SLOW;
+                type = ISpecialEffectInfo.TYPE_SPEED_VERY_SLOW;
                 break;
+            case "slow":
+                type = ISpecialEffectInfo.TYPE_SPEED_SLOW;
+                break;
+            case "fast":
+                type = ISpecialEffectInfo.TYPE_SPEED_FAST;
+                break;
+            case "very_fast":
+                type = ISpecialEffectInfo.TYPE_SPEED_VERY_FAST;
+                break;
+
+            case "slow_to_fast":
+                type = ISpecialEffectInfo.TYPE_SPEED_SLOW_TO_FAST;
+                break;
+            case "fast_to_slow":
+                type = ISpecialEffectInfo.TYPE_SPEED_FAST_TO_SLOW;
+                break;
+            case "normal_to_slow":
+                type = ISpecialEffectInfo.TYPE_SPEED_NORMAL_TO_SLOW;
+                break;
+            case "slow_to_normal":
+                type = ISpecialEffectInfo.TYPE_SPEED_SLOW_TO_NORMAL;
+                break;
+
+            case "double_zoom_in":
+                type = ISpecialEffectInfo.TYPE_DOUBLE_ZOOM_IN;
+                break;
+
+            case "split_screen_v2":
+                type = ISpecialEffectInfo.TYPE_SPLIT_SCREEN_V2;
+                break;
+            case "split_screen_v3":
+                type = ISpecialEffectInfo.TYPE_SPLIT_SCREEN_V3;
+                break;
+            case "split_screen_h2":
+                type = ISpecialEffectInfo.TYPE_SPLIT_SCREEN_H2;
+                break;
+            case "split_screen_h3":
+                type = ISpecialEffectInfo.TYPE_SPLIT_SCREEN_H3;
+                break;
+
             default:
                 throw new UnsupportedOperationException("wrong transition value = " + val);
         }
--- java/ffmpeg-merge-video/src/main/java/com/heaven7/ve/cross_os/ISpecialEffectInfo.java
+++ java/ffmpeg-merge-video/src/main/java/com/heaven7/ve/cross_os/ISpecialEffectInfo.java
@@ -6,15 +6,43 @@ package com.heaven7.ve.cross_os;
 public interface ISpecialEffectInfo extends IEffectInfo {
 
     //1缓慢缩小2缓慢放大3左平移4右平移5极慢(慢速)
-    int TYPE_SLOW_ZOOM_IN = 1;
-    int TYPE_SLOW_ZOOM_OUT = 2;
-    int TYPE_LEFT_MOVE = 3;
-    int TYPE_RIGHT_MOVE = 4;
-    int TYPE_VERY_SLOW = 5;
+    /** 无 */
+    int TYPE_NONE = 0;
+
+    int TYPE_ZOOM_OUT = 1;
+    int TYPE_ZOOM_IN  = 2;
+    int TYPE_LEFT_TRANSLATION = 3;
+    int TYPE_RIGHT_TRANSLATION = 4;
+
+    int TYPE_SPEED_SLOW      = 5;
+    int TYPE_SPEED_VERY_SLOW = 6;
+    int TYPE_SPEED_FAST      = 7;
+    int TYPE_SPEED_VERY_FAST = 8;
+
+    int TYPE_SPEED_SLOW_TO_FAST   = 9;
+    int TYPE_SPEED_FAST_TO_SLOW   = 10;
+    int TYPE_SPEED_NORMAL_TO_SLOW = 11;
+    int TYPE_SPEED_SLOW_TO_NORMAL = 12;
+
+    /** 变焦 */
+    int TYPE_DOUBLE_ZOOM_IN = 21;
+    /** 分屏 */
+    int TYPE_SPLIT_SCREEN      = 30;
+    /** 分屏: 竖直 2分 */
+    int TYPE_SPLIT_SCREEN_V2   = 31;
+    /** 分屏: 竖直 3分 */
+    int TYPE_SPLIT_SCREEN_V3   = 32;
+    /** 分屏: 水平 2分 */
+    int TYPE_SPLIT_SCREEN_H2   = 33;
+    /** 分屏: 水平 3分 */
+    int TYPE_SPLIT_SCREEN_H3   = 34;
 
     void setCategory(int category);
+
     int getCategory();
+
     void setMultiple(float multiple);
+
     float getMultiple();
 
 }
