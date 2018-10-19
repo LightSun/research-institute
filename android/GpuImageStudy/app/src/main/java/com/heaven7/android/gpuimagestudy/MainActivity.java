5a0ef743e606e154bd691d203db4687a12aa9ff3
lightsun <978136772@qq.com>
2018-10-19 20:25:42 +0800

    start gpu image study

---
+++ android/GpuImageStudy/app/src/main/java/com/heaven7/android/gpuimagestudy/MainActivity.java
@@ -0,0 +1,18 @@
+package com.heaven7.android.gpuimagestudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+    }
+
+}
