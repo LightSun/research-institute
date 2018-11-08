cbaa09459f646dfc71a4456536d0dc9b46cd404b
lightsun <978136772@qq.com>
2018-11-08 13:53:13 +0800

    no

--- dart/dart-study/bin/class_def.dart
+++ dart/dart-study/bin/class_def.dart
@@ -31,8 +31,9 @@ class Test2{
   static const initialCapacity = 16;
 }
 
-//with 可用于无参构造.
+//with:  可用于无参构造. 'Mixins 'are a way of reusing a class’s code in multiple class hierarchies.
 //dart 没有interface关键字。所有类都可以继承和 实现。
+//
 class Rect extends Object with Test, Test2 implements Invocation{
 
   int width;
--- dart/dart-study/bin/operator_override.dart
+++ dart/dart-study/bin/operator_override.dart
@@ -6,6 +6,8 @@ class Vector {
   Vector operator +(Vector v) => Vector(x + v.x, y + v.y);
   Vector operator -(Vector v) => Vector(x - v.x, y - v.y);
 
+  bool operator ==(other) => other is Vector && (x ==other.x && y ==other.y);
+
 // Operator == and hashCode not shown. For details, see note below.
 // ···
 
@@ -16,12 +18,22 @@ class Vector {
     print('You tried to use a non-existent member: ' +
         '${invocation.memberName}');
   }
+
+  @override
+  String toString() {
+    return 'Vector{x: $x, y: $y}';
+  }
+
 }
 
 void main() {
   final v = Vector(2, 3);
   final w = Vector(2, 2);
 
+  print(v + w);
   assert(v + w == Vector(4, 5));
   assert(v - w == Vector(0, 1));
+
+  var list = [1,2];
+  print(list.runtimeType); //list<int>
 }
