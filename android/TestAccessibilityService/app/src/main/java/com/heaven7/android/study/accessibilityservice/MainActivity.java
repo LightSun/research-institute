6b8dd5b893432ac6e60e037d2b4d4abbd41d9410
LightSun <donshine723@gmail.com>
2019-02-11 21:48:50 +0800

    test accessibility ok

---
+++ android/TestAccessibilityService/app/src/main/java/com/heaven7/android/study/accessibilityservice/MainActivity.java
@@ -0,0 +1,65 @@
+package com.heaven7.android.study.accessibilityservice;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.accessibility.AccessibilityManager;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startService(new Intent(this, TestAccessibilityService.class));
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public static boolean isRunning(AccessibilityService service) {
+        if(service == null) {
+            return false;
+        }
+        AccessibilityManager accessibilityManager = (AccessibilityManager) service.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        AccessibilityServiceInfo info = service.getServiceInfo();
+        if(info == null || accessibilityManager == null) {
+            return false;
+        }
+        List<AccessibilityServiceInfo> list = accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_GENERIC);
+        Iterator<AccessibilityServiceInfo> iterator = list.iterator();
+
+        boolean isConnect = false;
+        while (iterator.hasNext()) {
+            AccessibilityServiceInfo i = iterator.next();
+            if(i.getId().equals(info.getId())) {
+                isConnect = true;
+                break;
+            }
+        }
+        if(!isConnect) {
+            return false;
+        }
+        return true;
+    }
+
+    /** 快速读取通知栏服务是否启动*/
+   /* public static boolean isNotificationServiceRunning() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            return false;
+        }
+        //部份手机没有NotificationService服务
+        try {
+            return QHBNotificationService.isRunning();
+        } catch (Throwable t) {}
+        return false;
+    }*/
+
+}
