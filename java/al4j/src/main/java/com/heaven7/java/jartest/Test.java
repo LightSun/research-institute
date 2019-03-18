98b6b9527503b13651a9b7d6091b21799e27904a
lightsun <978136772@qq.com>
2019-03-18 17:28:27 +0800

    add al4j=android load image from java

---
+++ java/al4j/src/main/java/com/heaven7/java/jartest/Test.java
@@ -0,0 +1,12 @@
+package com.heaven7.java.jartest;
+
+/**
+ * @author heaven7
+ */
+public class Test {
+
+  public static void main(String[] args) {
+    //
+    System.out.println("-----------");
+  }
+}
