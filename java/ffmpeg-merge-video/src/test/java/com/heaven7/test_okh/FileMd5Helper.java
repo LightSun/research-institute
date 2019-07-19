37a72a2359d341e7f4083f265877812b3fc59299
lightsun <978136772@qq.com>
2019-07-19 17:42:20 +0800

    opt

--- java/ffmpeg-merge-video/src/test/java/com/heaven7/test_okh/FileMd5Helper.java
+++ java/ffmpeg-merge-video/src/test/java/com/heaven7/test_okh/FileMd5Helper.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
@@ -37,8 +38,22 @@ public class FileMd5Helper {
         return bi.toString(16);
     }
 
+    public static String getMD5Three2(String content) {
+        BigInteger bi = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] contentBytes = content.getBytes(StandardCharsets.UTF_8);
+            md.update(contentBytes, 0, contentBytes.length);
+            byte[] b = md.digest();
+            bi = new BigInteger(1, b);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return bi.toString(16);
+    }
+
     public static void main(String[] args) {
-        String dir = "D:\\Users\\Administrator\\AppData\\Local\\Temp\\media_files\\resource";
+       /* String dir = "D:\\Users\\Administrator\\AppData\\Local\\Temp\\media_files\\resource";
         List<String> files = FileUtils.getFiles(new File(dir), "mp4");
         String md5s = VisitServices.from(files).map(new ResultVisitor<String, String>() {
             @Override
@@ -46,7 +61,8 @@ public class FileMd5Helper {
                 return getMD5Three(s);
             }
         }).asListService().joinToString(",");
-        System.out.println(md5s);
-
+        System.out.println(md5s);*/
+        String str = "I love this sdkdfk dgfdgkfdgk sdgfd8gg89dg 788sdf9s sdf54345343523233523135%%&&(*(()*";
+        System.out.println(getMD5Three2(str));
     }
 }
