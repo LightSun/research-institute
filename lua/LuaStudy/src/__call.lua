dd63dbf2ebb8ad691a5704c73f2549402e1b8083
lightsun <978136772@qq.com>
2019-09-05 16:51:07 +0800

    ok

--- lua/LuaStudy/src/__call.lua
+++ lua/LuaStudy/src/__call.lua
@@ -6,18 +6,19 @@
 -- To change this template use File | Settings | File Templates.
 --
 
---元表  metatable
-local myTabel={'Lua','Java','C','C++'}
-local mymetatable={
-    --当把表当做方法来调用的时候，会执行__call对应的方法
-    __call = function (tab, arg)
-        print(arg)
-        return '调用了__call'
+----------------------------------------------------------------
+
+local list = {}
+local meta = {
+    __call = function (tab, ...)
+        local p = {...}
+        for i = 1 , #p do
+            print("arg"..i.." = "..tostring(p[i]))
+        end
     end
 }
-local newTabel={'Python','PHP','JavaScript'}
-myTabel = setmetatable(myTabel, mymetatable)--设置这个表的元表
+setmetatable(list, meta)
 
-local v = myTabel(55)
-print(v)
+local a = list(1, 2, 3)
+
 ---------------------
